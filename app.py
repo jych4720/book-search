33ada88230ec9c4348fa6bf1eb7e5c1ee8585c7d
jc4720 <jc4720@imperial.ac.uk>
2025-01-29 13:17:33 +0000

    final

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-from flask import Flask, jsonify, request
+import os
 import requests
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-BOOK_API_URL = "http://book-api.uksouth.azurecontainer.io:5000/api/books"
+BOOK_API_URL = os.getenv('BOOK_API_URL')
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
 
-@app.route('/books', methods=['GET'])
-def get_books():
+@app.route('/search', methods=['GET'])
+def search_books():
     genre = request.args.get('genre')
-    url = 'http://book-api.uksouth.azurecontainer.io:5000/api/books'
-    
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        books = response.json()
-        
-        # If a genre query parameter is provided, filter the books by genre
-        if genre:
-            filtered_books = [book for book in books if genre.lower() in book['genre'].lower()]
-            return jsonify(filtered_books)
-        
-        # If no genre parameter is provided, return the third book by default
-        else:
-            return jsonify(books[2])  # Return the third book (index 2)
+    author = request.args.get('author')
 
-if __name__ == '__main__':
+    params = {}
+    if genre:
+        params['genre'] = genre
+    if author:
+        params['author'] = author
+
+    try:
+        response = requests.get(BOOK_API_URL, params=params)
+        response.raise_for_status()
+        books = response.json()
+        return jsonify(books)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
